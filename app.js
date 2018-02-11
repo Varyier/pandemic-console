800ab66e9424d1bf8b3859576cb3f52311fb20a1
varik <attervip@yandex.ru>
2018-02-11 16:10:45 +0400

    t3

--- app.js
+++ app.js
@@ -2803,6 +2803,6 @@ app.post('/req', function(req, res) {
 //    });  
 //})
 
-var port = process.env.PORT || 5000;       
-app.listen(port)                           // Запускаем сервер на 5000 порту, если не указана переменная окружения "port" 
-console.log("Listening at " + port)        // Пишем в консоль, что запустились
+//var port = process.env.PORT || 5000;       
+app.listen(8080)                           // Запускаем сервер на 5000 порту, если не указана переменная окружения "port" 
+//console.log("Listening at " + port)        // Пишем в консоль, что запустились
